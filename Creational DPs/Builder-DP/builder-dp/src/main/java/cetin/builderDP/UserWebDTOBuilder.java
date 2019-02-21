2367512084fb372dfabc49a9a78dbc6fd8b4e56b
Ugurcan Cetin <ugurcan.cetin@ericsson.com>
2019-02-21 16:42:48 +0100

    [Builder-DP] Implementing Builder for Web User DTO

--- Creational DPs/Builder-DP/builder-dp/src/main/java/cetin/builderDP/UserWebDTOBuilder.java
+++ Creational DPs/Builder-DP/builder-dp/src/main/java/cetin/builderDP/UserWebDTOBuilder.java
@@ -1,4 +1,51 @@
 package cetin.builderDP;
 
-public class UserWebDTOBuilder {
+import java.time.LocalDate;
+import java.time.Period;
+
+public class UserWebDTOBuilder implements UserDTOBuilder{
+
+    private String firstName;
+    private String lastname;
+    private String age;
+    private String address;
+    private UserWebDTO dto;
+
+
+    @Override
+    public UserDTOBuilder withFirstName(String fname) {
+        this.firstName = fname;
+        return this;
+    }
+
+    @Override
+    public UserDTOBuilder withLastName(String lname) {
+        this.lastname = lname;
+        return this;
+    }
+
+    @Override
+    public UserDTOBuilder withBirthday(LocalDate date) {
+        Period ageInYears = Period.between(date, LocalDate.now());
+        this.age = Integer.toString(ageInYears.getYears());
+        return this;
+    }
+
+    @Override
+    public UserDTOBuilder withAddress(Address address) {
+        this.address = address.getHouseNumber() + ", "  + address.getStreet() + ", "
+                +"\n"+ address.getCity() +"\n"+ address.getState()+"\n"+ address.getZipcode();
+        return this;
+    }
+
+    @Override
+    public UserDTO build() {
+        dto = new UserWebDTO(firstName + " "+lastname, address, age);
+        return dto;
+    }
+
+    @Override
+    public UserDTO getUserDTO() {
+        return dto;
+    }
 }

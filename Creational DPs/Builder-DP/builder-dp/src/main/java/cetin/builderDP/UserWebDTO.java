58a7f0d57fff03414696a25ce01ec18c48483448
Ugurcan Cetin <ugurcan.cetin@ericsson.com>
2019-02-21 16:27:30 +0100

    [Builder-DP] Add another scenario for Web Users

---
+++ Creational DPs/Builder-DP/builder-dp/src/main/java/cetin/builderDP/UserWebDTO.java
@@ -0,0 +1,38 @@
+package cetin.builderDP;
+
+public class UserWebDTO implements UserDTO{
+
+    private String name;
+    private String address;
+    private String age;
+
+    public UserWebDTO(String name, String address, String age){
+        this.name = name;
+        this.address = address;
+        this.age = age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "UserWebDTO{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
---
+++ Creational DPs/Builder-DP/builder-dp/src/main/java/cetin/builderDP/UserWebDTOBuilder.java
@@ -0,0 +1,4 @@
+package cetin.builderDP;
+
+public class UserWebDTOBuilder {
+}

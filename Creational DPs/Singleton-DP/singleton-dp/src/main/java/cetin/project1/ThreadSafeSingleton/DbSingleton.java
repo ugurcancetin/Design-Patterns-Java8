fce2b244c5b8c68e1a91d88035c879fc3a7451a7
Ugurcan Cetin <ugurcan.cetin@ericsson.com>
2019-04-17 14:21:59 +0200

    Add little reminders

--- Creational DPs/Singleton-DP/singleton-dp/src/main/java/cetin/project1/ThreadSafeSingleton/DbSingleton.java
+++ Creational DPs/Singleton-DP/singleton-dp/src/main/java/cetin/project1/ThreadSafeSingleton/DbSingleton.java
@@ -5,6 +5,7 @@ public class DbSingleton {
     private static volatile DbSingleton instance = null;
 
     private DbSingleton() {
+        //protection thru reflection
         if(instance != null){
             throw new RuntimeException("Use getInstance() method to create...");
         }
@@ -13,6 +14,7 @@ public class DbSingleton {
     public static DbSingleton getInstance(){
         if(instance == null){
             synchronized (DbSingleton.class){
+                //for making it thread safe and protection for blocked threads
                 if(instance == null){
                     instance = new DbSingleton();
                 }

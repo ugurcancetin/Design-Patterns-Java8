d46773b75ec76fb158eb87b4e0984cdb2f0bd90f
ugurcancetin <cetinyazilimlar@gmail.com>
2019-10-24 11:49:08 +0200

    [Adapter-DP] Add class adapter

--- Structural DPs/Adapter-DP/src/main/java/cetin/Main.java
+++ Structural DPs/Adapter-DP/src/main/java/cetin/Main.java
@@ -2,7 +2,12 @@ package cetin;
 
 public class Main {
     public static void main(String[] args) {
+        EmployeeClassAdapter adapter = new EmployeeClassAdapter();
+        populateEmployeeData(adapter);
+        BusinessCardDesigner designer = new BusinessCardDesigner();
+        String card = designer.designCard(adapter);
 
+        System.out.println(card);
     }
 
     private static void populateEmployeeData(Employee employee) {

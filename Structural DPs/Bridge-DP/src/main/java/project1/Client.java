f80618f54eab424f30d633fa94a95a0f67b89041
ugurcancetin <cetinyazilimlar@gmail.com>
2019-11-04 16:30:56 +0100

    [Bridge-DP] Complete the example project

--- Structural DPs/Bridge-DP/src/main/java/project1/Client.java
+++ Structural DPs/Bridge-DP/src/main/java/project1/Client.java
@@ -1,7 +1,19 @@
 package project1;
 
+import project1.added.FifoCollection;
+import project1.added.Queue;
+import project1.existing.SinglyLinkedList;
+
 public class Client {
     public static void main(String[] args) {
-
+        FifoCollection<String> queue = new Queue(new SinglyLinkedList());
+        queue.offer("Ugurcan");
+        queue.offer("Cetin");
+        queue.offer("Harun");
+        queue.offer("Arpa");
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
     }
 }
--- Structural DPs/Bridge-DP/src/main/java/project1/added/Queue.java
+++ Structural DPs/Bridge-DP/src/main/java/project1/added/Queue.java
@@ -1,6 +1,23 @@
 package project1.added;
 
-//A refined abstraction.
-public class Queue<T> {
+import project1.existing.LinkedList;
 
+//A refined abstraction.
+public class Queue<T> implements FifoCollection<T>{
+
+    private LinkedList<T> linkedList;
+
+    public Queue(LinkedList<T> l) {
+        linkedList = l;
+    }
+
+    @Override
+    public void offer(T element) {
+        linkedList.addLast(element);
+    }
+
+    @Override
+    public T poll() {
+        return linkedList.removeFirst();
+    }
 }

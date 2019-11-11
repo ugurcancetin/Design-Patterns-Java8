3dffb2d9658f5546d0f5617441a0f951a1e16440
ugurcancetin <cetinyazilimlar@gmail.com>
2019-11-11 17:15:32 +0100

    [Decorator-DP] Sample of using decorator desing pattern

--- Structural DPs/Decorator-DP/src/main/java/project1/App.java
+++ Structural DPs/Decorator-DP/src/main/java/project1/App.java
@@ -2,7 +2,13 @@ package project1;
 
 public class App {
     public static void main(String[] args) {
-        Message message = new TextMessage("Test Message...");
+        Message message = new TextMessage("Test Message... <HEAD> - <BODY>");
         System.out.println(message.getContent());
+
+        Message decorator = new HtmlEncodedMessage(message);
+        System.out.println(decorator.getContent());
+
+        decorator = new Base64EncodedMessage(decorator);
+        System.out.println(decorator.getContent());
     }
 }
--- Structural DPs/Decorator-DP/src/main/java/project1/HtmlEncodedMessage.java
+++ Structural DPs/Decorator-DP/src/main/java/project1/HtmlEncodedMessage.java
@@ -1,5 +1,17 @@
 package project1;
 
+import org.apache.commons.text.StringEscapeUtils;
+
 //Decorator. Implements component interface
-public class HtmlEncodedMessage {
+public class HtmlEncodedMessage implements Message{
+    private Message msg;
+
+    public HtmlEncodedMessage(Message msg){
+        this.msg=msg;
+    }
+
+    @Override
+    public String getContent() {
+        return StringEscapeUtils.escapeHtml4(msg.getContent());
+    }
 }

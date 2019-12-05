e2e1543fe6bd2c43343be7ef6fc1475892921bd7
Ugurcan Cetin <cetinyazilimlar@gmail.com>
2019-12-05 17:48:59 +0100

    [Facade-DP] Implement Facade

--- Structural DPs/Facade-DP/src/main/java/com/cetin/Client.java
+++ Structural DPs/Facade-DP/src/main/java/com/cetin/Client.java
@@ -1,26 +1,15 @@
 package com.cetin;
 
-import com.cetin.email.*;
+import com.cetin.email.EmailFacade;
 
 public class Client {
     public static void main(String[] args) {
         Order order = new Order("101", 99.99);
 
-        boolean result = sendOrderEmailWithoutFacade(order);
+        boolean result = new EmailFacade().sendOrderEmail(order);
 
         System.out.println("Order Email "+ (result?"sent!":"NOT sent..."));
 
     }
 
-    private static boolean sendOrderEmailWithoutFacade(Order order) {
-        Template template = TemplateFactory.createTemplateFor(Template.TemplateType.Email);
-        Stationary stationary = StationaryFactory.createStationary();
-        Email email = Email.getBuilder()
-                .withTemplate(template)
-                .withStationary(stationary)
-                .forObject(order)
-                .build();
-        Mailer mailer = Mailer.getMailer();
-        return mailer.send(email);
-    }
 }
--- Structural DPs/Facade-DP/src/main/java/com/cetin/email/EmailFacade.java
+++ Structural DPs/Facade-DP/src/main/java/com/cetin/email/EmailFacade.java
@@ -1,5 +1,19 @@
 package com.cetin.email;
 
+import com.cetin.Order;
+
 //Facade provides simple methods for client to use
 public class EmailFacade {
+
+    public boolean sendOrderEmail(Order order){
+        Template template = TemplateFactory.createTemplateFor(Template.TemplateType.Email);
+        Stationary stationary = StationaryFactory.createStationary();
+        Email email = Email.getBuilder()
+                .withTemplate(template)
+                .withStationary(stationary)
+                .forObject(order)
+                .build();
+        Mailer mailer = Mailer.getMailer();
+        return mailer.send(email);
+    }
 }

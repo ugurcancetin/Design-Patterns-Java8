ddc682bbde551a066d96e13a942e4eb6059c8349
Ugurcan Cetin <cetinyazilimlar@gmail.com>
2019-12-05 12:07:28 +0100

    [Composite-DP] Implementation

--- Structural DPs/Composite-DP/src/main/java/com/cetin/BinaryFile.java
+++ Structural DPs/Composite-DP/src/main/java/com/cetin/BinaryFile.java
@@ -1,5 +1,31 @@
 package com.cetin;
 
 //Leaf node in composite pattern
-public class BinaryFile {
+public class BinaryFile extends File{
+    private long size;
+
+    public BinaryFile(String name, long size) {
+        super(name);
+        this.size = size;
+    }
+
+    @Override
+    public void ls() {
+        System.out.println(getName() +"\t"+ size);
+    }
+
+    @Override
+    public void addFile(File file) {
+        throw new UnsupportedOperationException("Leaf node does not support add operation");
+    }
+
+    @Override
+    public File[] getFiles() {
+        throw new UnsupportedOperationException("Leaf node does not support get operation");
+    }
+
+    @Override
+    public boolean removeFile(File file) {
+        throw new UnsupportedOperationException("Leaf node does not support remove operation");
+    }
 }
--- Structural DPs/Composite-DP/src/main/java/com/cetin/Directory.java
+++ Structural DPs/Composite-DP/src/main/java/com/cetin/Directory.java
@@ -1,5 +1,34 @@
 package com.cetin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //Composite in the composite pattern
-public class Directory {
+public class Directory extends File{
+    private List<File> children = new ArrayList<>();
+
+    public Directory(String name) {
+        super(name);
+    }
+
+    @Override
+    public void ls() {
+        System.out.println(getName());
+        children.forEach(File::ls);
+    }
+
+    @Override
+    public void addFile(File file) {
+        children.add(file);
+    }
+
+    @Override
+    public File[] getFiles() {
+        return children.toArray(new File[children.size()]);
+    }
+
+    @Override
+    public boolean removeFile(File file) {
+        return children.remove(file);
+    }
 }
